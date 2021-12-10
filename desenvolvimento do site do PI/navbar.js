d31c586a0d8cc5b02a3845c2f0766222ace459d0
Domwilys <94638288+Domwilys@users.noreply.github.com>
2021-12-09 21:49:47 -0300

    Add files via upload

---
+++ desenvolvimento do site do PI/navbar.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('nav');
+    if(window.pageYOffset > 0){
+        navbar.classList.add('nav2');
+    }else {
+        navbar.classList.remove('nav2');
+    }
+});
