7b7b46736117128228486ff09ed97576373c4ffc
Domwilys <94638288+Domwilys@users.noreply.github.com>
2021-12-13 00:43:21 -0300

    Delete desenvolvimento do site do PI directory

--- desenvolvimento do site do PI/navbar.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    let navbar = document.querySelector('nav');
-    if(window.pageYOffset > 0){
-        navbar.classList.add('nav2');
-    }else {
-        navbar.classList.remove('nav2');
-    }
-});
